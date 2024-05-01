51d440a6315dd81dfcca8288d3419e9f086c23be
thinkAmi <dev.thinkami+git@gmail.com>
2024-05-01 20:30:36 +0900

    add migrate

---
+++ migrate.ts
@@ -0,0 +1,10 @@
+import { migrate } from "drizzle-orm/bun-sqlite/migrator"
+
+import { drizzle } from "drizzle-orm/bun-sqlite"
+import { Database } from "bun:sqlite"
+
+const sqlite = new Database("sqlite.db")
+const db = drizzle(sqlite)
+
+// [Ignore] TS80007: await has no effect on the type of this expression.
+await migrate(db, { migrationsFolder: "./drizzle" })
