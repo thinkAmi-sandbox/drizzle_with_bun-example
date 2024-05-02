b8d7fd5466bd736505d627f25cd3321d1502f000
thinkAmi <dev.thinkami+git@gmail.com>
2024-05-02 16:58:54 +0900

    add default constraints

---
+++ drizzle/0008_fuzzy_logan.sql
@@ -0,0 +1,1 @@
+ALTER TABLE writers ADD `comment` text DEFAULT '';
--- src/schema/writers.ts
+++ src/schema/writers.ts
@@ -3,7 +3,8 @@ import {text, integer, sqliteTable, uniqueIndex} from 'drizzle-orm/sqlite-core'
 export const writers = sqliteTable("writers", {
   id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
   name: text("name"),
-  pseudonym: text("pseudonym").notNull()
+  pseudonym: text("pseudonym").notNull(),
+  comment: text("comment").default("")
 }, (writer) => ({
   uniqueIdx: uniqueIndex("unique_pseudonym").on(writer.pseudonym)
 }))
